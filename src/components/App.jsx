eed22686c86dea435e603aa90de0682a871a229c
Yuki <yuki@YukideMacBook-Pro.local>
2021-07-16 15:15:51 +0800

    add image

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import "../styles/index.scss"
 
 import Recipes from './Recipes'
+import sword from "../images/swc-sword.png"
+import swordSvg from '../images/sword.svg'
 
 const App = () => {
     return(
@@ -10,8 +12,10 @@ const App = () => {
                <section>
                    <h1>Oh hi, React</h1>
                </section>
+               <img src={sword} alt="sword" width="250" />
+               <img src={swordSvg} alt="sword" width="250" />
+               <Recipes />
            </main>
-           <Recipes />
        </div>
     )
 }
