73989c727824eeb57481663e5f249a8aeb89b9ed
Yuki <yuki@YukideMacBook-Pro.local>
2021-07-16 10:34:40 +0800

    add css modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 let mode = "development"
 
 if (process.env.NODE_ENV === 'production') {
@@ -8,15 +9,23 @@ module.exports = {
     mode: mode,
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
+                test: /\.s?css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
             }
-        }]
+        ]
     },
+
+    plugins: [new MiniCssExtractPlugin()],
     devtool: "source-map",
     devServer: {
         contentBase: "./dist",
+        hot: true,
     },
 }
